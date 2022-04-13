b66528f6d6e91773abd12dccc7b806f706865ce6
Sunki-Kim <83565313+skdding3@users.noreply.github.com>
2022-04-13 09:12:42 +0900

    데이터

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log('Sunki!');
+let myName = "Sunki";
+let email = 'skdding3@gmail.com'
+let hello = `Hello ${myName}?!` // `를 사용해서 보간법 가능
+
+console.log(myName);
+console.log(email);
+console.log(hello);
