05c181c99a69a4dfe426dd66bd04085427e01daf
Sunki-Kim <83565313+skdding3@users.noreply.github.com>
2022-04-18 03:19:54 +0900

    스크롤

---
+++ css/scroll event.js
@@ -0,0 +1,22 @@
+// scroll event
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  } else {
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300));
+// _.throttle(함수, 시간)
+
+
+//cdn gsap
